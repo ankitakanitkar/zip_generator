0faf5c05c040e38756332c21ec7b97fa6c2ed360
Ankita Kanitkar <ankitakanitkar@gmail.com>
2015-07-06 16:44:07 +0100

    added generate method for array of files

--- lib/zip_generator.rb
+++ lib/zip_generator.rb
@@ -1,5 +1,18 @@
 require "zip_generator/version"
+require "zip"
+require "zip/zipfilesystem"
 
 module ZipGenerator
   # Your code goes here...
+  def generate_zip_files files 
+  	#files should be an array of Hash {data: file_content ,name: file_name}
+  	stringio = Zip::ZipOutputStream::write_buffer do |zio|
+  		files.each do |key|
+  			zio.put_next_entry(key[:name])
+  			zio.write key[:data]
+  		end
+  	end
+  	stringio.rewind
+  	stringio.sysread
+  end
 end
