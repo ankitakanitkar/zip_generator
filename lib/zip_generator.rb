5bcaaa61290f42ca75621641c9a1964e834e8967
hemalijain <hemalijains@gmail.com>
2015-07-10 05:40:40 -0700

    added as class method

--- lib/zip_generator.rb
+++ lib/zip_generator.rb
@@ -4,15 +4,15 @@ require "zip/zipfilesystem"
 
 module ZipGenerator
   # Your code goes here...
-  def generate_zip_files files 
+  def self.generate_zip_files files 
   	#files should be an array of Hash {data: file_content ,name: file_name}
   	stringio = Zip::ZipOutputStream::write_buffer do |zio|
-  		files.each do |key|
-  			zio.put_next_entry(key[:name])
-  			zio.write key[:data]
-  		end
-  	end
-  	stringio.rewind
-  	stringio.sysread
+        files.each do |key|
+          zio.put_next_entry(key['name'])
+          zio.write key['data']
+        end
+      end
+      stringio.rewind
+      stringio.sysread
   end
 end
