4ce4b7f14e11c9852596bec95d33db93a8185f73
Ankita Kanitkar <Ankita_Kanitkar@external.mckinsey.com>
2015-07-13 20:43:48 +0530

    added specs and rescue

--- lib/zip_generator.rb
+++ lib/zip_generator.rb
@@ -6,13 +6,21 @@ module ZipGenerator
   # Your code goes here...
   def self.generate_zip_files files 
   	#files should be an array of Hash {data: file_content ,name: file_name}
-  	stringio = Zip::ZipOutputStream::write_buffer do |zio|
-        files.each do |key|
-          zio.put_next_entry(key['name'])
-          zio.write key['data']
+  	begin
+      stringio = Zip::ZipOutputStream::write_buffer do |zio|
+          files.each do |key|
+              if key.has_key?('name'.to_sym)
+                zio.put_next_entry(key['name'])
+                zio.write key['data']
+              else
+                raise "Format Error"
+              end
+          end
         end
-      end
-      stringio.rewind
-      stringio.sysread
+        stringio.rewind
+        stringio.sysread
+    rescue => msg
+      msg
+    end
   end
 end
--- spec/zip_generator_spec.rb
+++ spec/zip_generator_spec.rb
@@ -4,9 +4,15 @@ require 'spec_helper'
 describe ZipGenerator do
 
  it "should return a zipped file containing all the files supplied as array" do
- 	file1 = File.open("file1.txt", "w+") {|f| f.write("File Content for file 1") }
- 	file2 = File.open("file2.txt", "w+") {|f| f.write("File content for file 2") }
- 	file_arr = [file1 , file2]
- 	expect {:generate_zip_file(file_arr).to_return true }
+ 	file1 = {data: File.open("file1.txt", "w+") {|f| f.write("File Content for file 1") } , name: "file1.txt"}
+ 	file2 = {data: File.open("file2.txt", "w+") {|f| f.write("File content for file 2") } , name: "file2.txt"}
+ 	file_arr = [file1,file2]
+ 	zipped = ZipGenerator.generate_zip_files(file_arr)
+ 	zipped.should be_a_kind_of(String) 
+ end
+ it "should raise error" do
+ 	file1 = {content: File.open("file1.txt", "w+") {|f| f.write("File Content for file 1")} }
+ 	zipped = ZipGenerator.generate_zip_files([file1])
+ 	zipped.should be_a_kind_of(RuntimeError) 
  end
 end
